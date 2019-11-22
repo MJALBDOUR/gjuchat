fb14225628347c4867a627ac8502f629d7f9a8c3
mjalbdour <mjalbdour@gmail.com>
2019-11-22 19:14:05 +0200

    restructuring admin model

--- models/adminModel.js
+++ models/adminModel.js
@@ -1,52 +1,106 @@
 "use strict";
+
 const mongoose = require("mongoose"),
-  { Schema } = require("mongoose"),
-  passport = require("passport-local-mongoose");
+  { Schema } = require("mongoose");
 
 let adminSchema = new Schema(
   {
-    idNumber: {
-      type: Number,
+    personalInformation: {
       required: true,
-      unique: true
-    },
-    name: {
-      first: {
+      adminId: {
+        type: Number,
+        min: 0,
+        max: 100,
+        required: true,
+        unique: true
+      },
+      name: {
+        required: true,
         type: String,
         trim: true,
+        firstName,
+        fatherName,
+        grandfatherName,
+        lastName
+      },
+      username: {
+        type: String,
+        lowercase: true,
+        required: [true, "can't be blank"],
+        match: [/^[a-zA-Z0-9]+$/, "is invalid"],
+        index: true,
+        unique: true
+      },
+      nationality: {
+        type: String,
         required: true
       },
-      last: {
+      gender: {
         type: String,
-        trim: true,
         required: true
+      },
+      birthdate: {
+        type: Date,
+        required: true
+      },
+      religion: {
+        type: String
+      },
+      maritialStatus: {
+        type: String,
+        default: "single"
       }
     },
-    email: {
-      type: String,
-      lowercase: true,
+    contactInformation: {
       required: true,
-      unique: true
-    },
-
-    passwordHash: {
-      type: String,
-      required: true
-    },
-
-    messages: [{ type: Schema.Types.ObjectId, ref: "Message" }]
+      hashPassword: {
+        type: String
+      },
+      gjuEmail: {
+        type: String,
+        lowercase: true,
+        required: [true, "can't be blank"],
+        match: [/\S+@\S+\.\S+/, "is invalid"],
+        index: true,
+        unique: true
+      },
+      secondaryEmail: {
+        type: String,
+        lowercase: true,
+        required: [true, "can't be blank"],
+        match: [/\S+@\S+\.\S+/, "is invalid"],
+        unique: true
+      },
+      mobileNumber: {
+        type: Number,
+        unique: true
+      },
+      address: {
+        street: {
+          type: String
+        },
+        city: {
+          type: String,
+          default: "amman"
+        },
+        zipCode: {
+          type: Number,
+          min: [00000, "zip code too low"],
+          max: [99999, "zip code too high"]
+        }
+      }
+    }
   },
   {
     timestamps: true
   }
 );
 
-adminSchema.virtual("fullName", function() {
-  return `${this.name.first} ${this.name.last}`;
+adminSchema.virtual("firstLastName").get(function() {
+  return `${this.personalInformation.name.firstName} ${this.personalInformation.name.lastName}`;
 });
 
-studentSchema.plugin(passport, {
-  usernameField: "email"
+adminSchema.virtual("fullName").get(function() {
+  return `${this.personalInformation.name.firstName} ${this.personalInformation.name.fatherName} ${this.personalInformation.name.grandfatherName} ${this.personalInformation.name.lastName}`;
 });
-
 module.exports = mongoose.model("Admin", adminSchema);
