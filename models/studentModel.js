fc7fea5d84dd3b303555ab70f91febc9ac4ceb36
mjalbdour <mjalbdour@gmail.com>
2019-11-22 18:04:36 +0200

    modifying the student model to a better Schema

---
+++ models/studentModel.js
@@ -0,0 +1,102 @@
+"use strict";
+
+const mongoose = require("mongoose"),
+  { Schema } = require("mongoose");
+
+let studentSchema = new Schema(
+  {
+    personalInformation: {
+      required: true,
+      studentId: {
+        type: Number,
+        min: 20050000000,
+        max: 29999999999,
+        required: true
+      },
+      name: {
+        required: true,
+        type: String,
+        trim: true,
+        firstName,
+        fatherName,
+        grandfatherName,
+        lastName
+      },
+      username: {
+        type: String,
+        lowercase: true,
+        required: [true, "can't be blank"],
+        match: [/^[a-zA-Z0-9]+$/, "is invalid"],
+        index: true,
+        unique: true
+      },
+      nationality: {
+        type: String,
+        required: true
+      },
+      gender: {
+        type: String,
+        required: true
+      },
+      birthdate: {
+        type: Date,
+        required: true
+      },
+      religion: {
+        type: String
+      },
+      maritialStatus: {
+        type: String,
+        default: "single"
+      }
+    },
+    contactInformation: {
+      required: true,
+      gjuEmail: {
+        type: String,
+        lowercase: true,
+        required: [true, "can't be blank"],
+        match: [/\S+@\S+\.\S+/, "is invalid"],
+        index: true,
+        unique: true
+      },
+      secondaryEmail: {
+        type: String,
+        lowercase: true,
+        required: [true, "can't be blank"],
+        match: [/\S+@\S+\.\S+/, "is invalid"],
+        index: true,
+        unique: true
+      },
+      mobileNumber: {
+        type: Number
+      },
+      address: {
+        street: {
+          type: String
+        },
+        City: {
+          type: String,
+          default: "amman"
+        },
+        zipCode: {
+          type: Number,
+          min: [00000, "zip code too low"],
+          max: [99999, "zip code too high"]
+        }
+      }
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+studentSchema.virtual("firstLastName").get(function() {
+  return `${this.personalInformation.name.firstName} ${this.personalInformation.name.last}`;
+});
+
+studentSchema.virtual("fullName").get(function() {
+  return `${this.personalInformation.name.firstName} ${this.personalInformation.name.fatherName} ${this.personalInformation.name.grandfatherName} ${this.personalInformation.name.last}`;
+});
+module.exports = mongoose.model("Student", studentSchema);
