44a0e8e25728a7e16a0ecfde8982b669f461a6b5
mjalbdour <mjalbdour@gmail.com>
2019-11-22 19:07:48 +0200

    restructuring the teacher model

--- models/teacherModel.js
+++ models/teacherModel.js
@@ -1,54 +1,128 @@
 "use strict";
+
 const mongoose = require("mongoose"),
-  { Schema } = require("mongoose"),
-  passport = require("passport-local-mongoose");
+  { Schema } = require("mongoose");
 
 let teacherSchema = new Schema(
   {
-    idNumber: {
-      type: Number,
+    personalInformation: {
       required: true,
-      unique: true,
-    },
-
-    name: {
-      first: {
+      teacherId: {
+        type: Number,
+        min: 20050000000,
+        max: 29999999999,
+        required: true,
+        unique: true
+      },
+      name: {
+        required: true,
         type: String,
         trim: true,
+        firstName,
+        fatherName,
+        grandfatherName,
+        lastName
+      },
+      username: {
+        type: String,
+        lowercase: true,
+        required: [true, "can't be blank"],
+        match: [/^[a-zA-Z0-9]+$/, "is invalid"],
+        index: true,
+        unique: true
+      },
+      nationality: {
+        type: String,
         required: true
       },
-      last: {
+      gender: {
         type: String,
-        trim: true,
         required: true
+      },
+      birthdate: {
+        type: Date,
+        required: true
+      },
+      religion: {
+        type: String
+      },
+      maritialStatus: {
+        type: String,
+        default: "single"
       }
     },
-    email: {
-      type: String,
-      lowercase: true,
+    academicInformation: {
       required: true,
-      unique: true
+      degree: {
+        type: String,
+        default: "master"
+      },
+      faculty: {
+        type: String
+      },
+      department: {
+        type: String
+      },
+      major: {
+        type: String
+      },
+      courses: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: "Course"
+        }
+      ]
     },
-
-    passwordHash: {
-      type: String,
-      required: true
-    },
-
-    courses: [{ type: Schema.Types.ObjectId, ref: "Course" }],
-    messages: [{ type: Schema.Types.ObjectId, ref: "Message" }]
+    contactInformation: {
+      required: true,
+      hashPassword: {
+        type: String
+      },
+      gjuEmail: {
+        type: String,
+        lowercase: true,
+        required: [true, "can't be blank"],
+        match: [/\S+@\S+\.\S+/, "is invalid"],
+        index: true,
+        unique: true
+      },
+      secondaryEmail: {
+        type: String,
+        lowercase: true,
+        required: [true, "can't be blank"],
+        match: [/\S+@\S+\.\S+/, "is invalid"],
+        unique: true
+      },
+      mobileNumber: {
+        type: Number,
+        unique: true
+      },
+      address: {
+        street: {
+          type: String
+        },
+        City: {
+          type: String,
+          default: "amman"
+        },
+        zipCode: {
+          type: Number,
+          min: [00000, "zip code too low"],
+          max: [99999, "zip code too high"]
+        }
+      }
+    }
   },
   {
     timestamps: true
   }
 );
 
-teacherSchema.virtual("fullName", function() {
-  return `${this.name.first} ${this.name.last}`;
+teacherSchema.virtual("firstLastName").get(function() {
+  return `${this.personalInformation.name.firstName} ${this.personalInformation.name.lastName}`;
 });
 
-teacherSchema.plugin(passport, {
-  usernameField: "email"
+teacherSchema.virtual("fullName").get(function() {
+  return `${this.personalInformation.name.firstName} ${this.personalInformation.name.fatherName} ${this.personalInformation.name.grandfatherName} ${this.personalInformation.name.lastName}`;
 });
-
 module.exports = mongoose.model("Teacher", teacherSchema);
