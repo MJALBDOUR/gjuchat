f27360d5d7688173b421845afb678a9dcf650c5f
mjalbdour <mjalbdour@gmail.com>
2019-12-28 18:26:49 +0200

    setting up basic welcome/login files student or admin

---
+++ flutter_app/gjuchat_app/lib/user_types/student/screens/student_login.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class UserTypes extends StatefulWidget {
+  @override
+  _UserTypesState createState() => _UserTypesState();
+}
+
+class _UserTypesState extends State<UserTypes> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
